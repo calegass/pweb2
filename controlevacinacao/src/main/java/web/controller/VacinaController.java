2e81ef334710138501368d276f85963b4c2aafc9
calegass <matheuscm7@hotmail.com>
2024-09-19 13:33:31 -0300

    aula 19 de set

--- controlevacinacao/src/main/java/web/controller/VacinaController.java
+++ controlevacinacao/src/main/java/web/controller/VacinaController.java
@@ -1,8 +1,11 @@
 package web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import web.model.Vacina;
 import web.repository.VacinaRepository;
@@ -12,7 +15,8 @@ import java.util.List;
 @Controller
 @RequestMapping("/vacinas")
 public class VacinaController {
-    private VacinaRepository repository;
+    private Logger logger = LoggerFactory.getLogger(VacinaController.class);
+    private final VacinaRepository repository;
 
     public VacinaController(VacinaRepository repository) {
         this.repository = repository;
@@ -22,7 +26,29 @@ public class VacinaController {
     public String todasVacinas(Model model) {
         List<Vacina> vacinas = repository.findAll();
         model.addAttribute("vacinas", vacinas);
-        
+
+//        logger.info("Exibindo todas as vacinas: {}", vacinas);
+
         return "vacinas/todas";
+    }
+
+    @GetMapping("/nova")
+    public String abrirCadastroVacina(Model model) {
+        Vacina vacina = new Vacina();
+        model.addAttribute("vacina", vacina);
+
+        return "vacinas/nova";
+    }
+
+    @PostMapping("/nova")
+    public String cadastrarVacina(Vacina vacina) {
+        repository.save(vacina);
+
+        return "redirect:/vacinas/sucesso";
+    }
+
+    @GetMapping("/sucesso")
+    public String sucessoCadastro() {
+        return "vacinas/sucesso";
     }
 }
