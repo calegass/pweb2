004d6a892e8ad50a43f890d43377cf301e50f599
calegass <matheuscm7@hotmail.com>
2024-12-11 22:04:35 -0300

    fix: rollback http port to 8080 (was 8081)

--- controlecarros/src/main/java/web/controlecarros/Application.java
+++ controlecarros/src/main/java/web/controlecarros/Application.java
@@ -37,7 +37,7 @@ public class Application {
 	private Connector redirectConnector() {
 		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
 		connector.setScheme("http");
-		connector.setPort(8081);
+		connector.setPort(8080);
 		connector.setSecure(false);
 		connector.setRedirectPort(8443);
 		return connector;
